dcacac1f6f61cd6e3c096ba9690d7a4138c4d197
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-02 06:54:03 +0530

    Functions

--- 04-functions.sh
+++ 04-functions.sh
@@ -4,3 +4,13 @@ example() {
 
 # call function
 example
+
+# Functions have its own special variables
+
+example1() {
+  echo Value of 1 - $1
+  echo "Value of * - $*"
+  echo "Value of # - $#"
+}
+
+example1 10 20 30
