adfcf84b74ce3490927effb3be4742108f361d20
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-04 06:24:01 +0530

    If condition

---
+++ 05-if.sh
@@ -0,0 +1,72 @@
+## Example for multi line comment
+<<EOF
+simple if
+
+if [ expression ]; then
+  commands
+fi
+
+If Else
+
+if [ expression ]; then
+  commands
+else
+  commands
+fi
+
+Else If
+
+if [ expression1 ]; then
+  commands
+elif [ expression2 ]; then
+  commands
+else
+  commands
+fi
+
+EOF
+
+# Expressions
+
+# 1 Number Expression
+
+<<EOF
+
+  Operators:
+
+  -eq
+  -ne
+  -gt
+  -ge
+  -lt
+  -le
+
+  [ 1 -eq 1 ]
+  a=10
+  [ $a -eq 5 ]
+EOF
+
+# 2. String Expression
+
+<<EOF
+  Operators:
+    = , ==
+    -z
+    !=
+
+    [ abc == "$xyz"]
+    [ -z "$VAR" ]
+
+EOF
+
+# 3. File Expression
+
+# -e , [ -e /etc/file ]
+
+
+x=10
+if [ $x -gt 100 ]; then
+  echo " $x is greather than 100"
+else
+  echo " $x is less than 100"
+fi
